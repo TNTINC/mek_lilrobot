9ba9aec77858bfc9704f84b92bb7f478cb7ffbde
Fridfinnur M <notandinotandi@gmail.com>
2023-04-02 18:32:27 +0000

    interstitial

--- src/mek_lilrobot/launch/launch_sim.launch.py
+++ src/mek_lilrobot/launch/launch_sim.launch.py
@@ -4,8 +4,9 @@ from ament_index_python.packages import get_package_share_directory
 
 
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, TimerAction, RegisterEventHandler
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.event_handlers import OnProcessStart, OnProcessExit
 
 from launch_ros.actions import Node
 
@@ -31,8 +32,9 @@ def generate_launch_description():
     gazebo = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
-                    launch_arguments={'world':os.path.join(
-                    get_package_share_directory(package_name),'worlds','arena.sdf'),'extra_gazebo_args': '--ros-args --params-file ' + gazebo_params_path}.items()
+                    launch_arguments={
+        #'world':os.path.join(get_package_share_directory(package_name),'worlds','arena.sdf'),
+        'extra_gazebo_args': '--ros-args --params-file ' + gazebo_params_path}.items()
              )
 
     # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
@@ -53,12 +55,23 @@ def generate_launch_description():
         arguments=["joint_broad"],
     )
 
+    delayed_spawn_dc = RegisterEventHandler(
+        event_handler=OnProcessExit(
+        target_action=spawn_entity,
+        on_exit=[diff_drive_spawner])
+    )
+
+    delayed_spawn_jb = RegisterEventHandler(
+        event_handler=OnProcessExit(
+        target_action=diff_drive_spawner,
+        on_exit=[joint_broad_spawner])
+    )
 
     # Launch them all!
     return LaunchDescription([
         rsp,
         gazebo,
         spawn_entity,
-        diff_drive_spawner,
-        joint_broad_spawner
+        delayed_spawn_jb,
+        delayed_spawn_dc
     ])

3c6e5df467361e38a77ef5efb2109cf6a9879414
Fridfinnur M <notandinotandi@gmail.com>
2023-04-09 21:56:55 +0000

    Add service to publish resettable map->odom tf

---
+++ src/mek_lilrobot/src/robot.cpp
@@ -0,0 +1,91 @@
+#include <functional>
+
+#include "rclcpp/rclcpp.hpp"
+
+#include "geometry_msgs/msg/transform_stamped.hpp"
+#include "geometry_msgs/msg/pose.hpp"
+#include "std_srvs/srv/empty.hpp"
+#include "tf2_ros/static_transform_broadcaster.h"
+#include "tf2_ros/transform_listener.h"
+#include "tf2_ros/buffer.h"
+#include "tf2/LinearMath/Transform.h"
+#include "tf2_geometry_msgs/tf2_geometry_msgs.hpp"
+#include "tf2/convert.h"
+
+/*
+ * [X] Publish map -> odom transform
+ * [X] Allow resetting map->odom transform
+ * [ ] Host state machine (search -> aquire -> return)
+ */
+using namespace std::placeholders;
+class Robot : public rclcpp::Node {
+    using TransformStamped = geometry_msgs::msg::TransformStamped;
+    using Empty_Response = std_srvs::srv::Empty_Response;
+    using Empty_Request = std_srvs::srv::Empty_Request;
+
+    // Fields
+    std::unique_ptr<tf2_ros::StaticTransformBroadcaster> tf_broadcaster;
+    std::unique_ptr<tf2_ros::Buffer> tf_buffer;
+    std::shared_ptr<tf2_ros::TransformListener> tf_listener;
+    std::shared_ptr<rclcpp::Service<std_srvs::srv::Empty>> reset_odom_srv;
+
+    public:
+    Robot() : Node("robot") {
+        // map -> odom transform broadcaster
+        auto qos_profile = rmw_qos_profile_default;
+        qos_profile.durability = RMW_QOS_POLICY_DURABILITY_TRANSIENT_LOCAL;
+        auto qos = rclcpp::QoS(rclcpp::KeepLast(1), qos_profile);
+        tf_broadcaster = std::make_unique<tf2_ros::StaticTransformBroadcaster>(*this, qos);
+
+        // Send initial 0,0,0 0,0,0,0 map->odom transform
+        auto init_tf = TransformStamped();
+        init_tf.header.stamp = this->get_clock()->now();
+        init_tf.header.frame_id = "map";
+        init_tf.child_frame_id = "odom";
+        tf_broadcaster->sendTransform(init_tf);
+
+        // transform listener to get odom and base_link
+        tf_buffer = std::make_unique<tf2_ros::Buffer>(this->get_clock());
+        tf_listener = std::make_shared<tf2_ros::TransformListener>(*tf_buffer);
+
+        // Service to set map->odom such that map->base_link = 0,0,0 0,0,0,0
+        reset_odom_srv = this->create_service<std_srvs::srv::Empty>(
+            "/robot/reset_odom", std::bind(&Robot::reset_odom, this, _1, _2, _3));
+        RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Node constructor finished");
+    }
+
+    private:
+
+    // Service handler, sets the map->odom transform such that map->base_link = 0,0,0 0,0,0,0
+    void reset_odom(
+        const std::shared_ptr<rmw_request_id_t> hdr,
+        const std::shared_ptr<Empty_Request> req,
+        std::shared_ptr<Empty_Response> res
+        ){ 
+        (void)hdr; (void)req; (void)res;
+        RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Reset odom service callback");
+
+        tf2::Transform odom_to_base_link{};
+        tf2::fromMsg(
+            tf_buffer->lookupTransform("odom", "base_link", tf2::TimePointZero).transform, 
+            odom_to_base_link);
+
+        auto map_to_odom = odom_to_base_link.inverse();
+
+        auto map_to_odom_msg = TransformStamped();
+        map_to_odom_msg.header.stamp = this->get_clock()->now();
+        map_to_odom_msg.header.frame_id = "map";
+        map_to_odom_msg.child_frame_id = "odom";
+        map_to_odom_msg.transform = tf2::toMsg(map_to_odom);
+        this->tf_broadcaster->sendTransform(map_to_odom_msg);
+    }
+
+};
+
+int main(int argc, char* argv[])
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<Robot>());
+  rclcpp::shutdown();
+  return 0;
+}
